e404bc8a9edc42d71b30aa7615ae08372fb051f6
DC <43692686+excaliburDC@users.noreply.github.com>
2020-06-12 11:35:55 +0530

    Updated player spawning system

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -5,11 +5,23 @@ public class PlayerSpawner : MonoBehaviour
 {
 
     public GameObject player1Prefab;
+    public GameObject player2Prefab;
+
+    private Vector3 offset = new Vector3(2f, 0f, 0f);
 
 
     void Start()
     {
-        PhotonNetwork.Instantiate(player1Prefab.name, transform.position, Quaternion.identity);
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(player1Prefab.name, transform.position+offset, Quaternion.identity);
+        }
+
+        else
+        {
+            PhotonNetwork.Instantiate(player2Prefab.name, transform.position-offset, Quaternion.identity);
+        }
+        
     }
 
     
